44698f338887adb5f1fc11abc63f7308e5c58bda
ouzy <ousmane.mariko89@gmail.com>
2016-01-25 15:22:28 -0500

    progress bar

--- prod/js/header.js
+++ prod/js/header.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
 	$(".nav-list a").click(function(){
 		$("input#showblock").prop('checked', false);
 	});
+
+	var winHeight = $(window).height(), 
+      	docHeight = $(document).height(),
+      	progressBar = $('progress'),
+      	max, value;
+
+	/* Set the max scrollable area */
+	max = docHeight - winHeight;
+	progressBar.attr('max', max);
+
+	$(document).on('scroll', function(){
+	    value = $(window).scrollTop();
+	    progressBar.attr('value', value);
+	});
 });
