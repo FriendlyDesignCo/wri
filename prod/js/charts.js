da33c4a926e70ccdad21e1cb1213adaa91bafe6d
ouzy <ousmane.mariko89@gmail.com>
2016-02-04 13:01:17 -0500

    chart no. 2

--- prod/js/charts.js
+++ prod/js/charts.js
@@ -1,70 +1,129 @@
 $(function () {
-
-            $('#chartContainerES1').highcharts({  
-                chart: {
-                    type: 'area'
-                },
-                exporting: {
-                    chartOptions: { // specific options for the exported image
-                        plotOptions: {
-                            series: {
-                                dataLabels: {
-                                    enabled: true
-                                }
-                            }
-                        }
-                    },
-                    scale: 3,
-                    fallbackToExportServer: false
-                },
-                title: {
-                    text: 'Historic and Estimated Worldwide Population Distribution by Region'
-                },
-                subtitle: {
-                    text: 'Source: Wikipedia.org'
-                },
-                xAxis: {
-                    categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-                    tickmarkPlacement: 'on',
-                    title: {
-                        enabled: false
-                    }
-                },
-                yAxis: {
-                    title: {
-                        text: 'Percent'
-                    }
-                },
-                tooltip: {
-                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.1f}%</b> ({point.y:,.0f} millions)<br/>',
-                    shared: true
-                },
+    // files with all the charts
+    $('#chartContainerES1').highcharts({  
+        chart: {
+            type: 'area'
+        },
+        exporting: {
+            chartOptions: { // specific options for the exported image
                 plotOptions: {
-                    area: {
-                        stacking: 'percent',
-                        lineColor: '#ffffff',
-                        lineWidth: 1,
-                        marker: {
-                            lineWidth: 1,
-                            lineColor: '#ffffff'
+                    series: {
+                        dataLabels: {
+                            enabled: true
                         }
                     }
-                },
-                series: [{
-                    name: 'Asia',
-                    data: [502, 635, 809, 947, 1402, 3634, 5268]
-                }, {
-                    name: 'Africa',
-                    data: [106, 107, 111, 133, 221, 767, 1766]
-                }, {
-                    name: 'Europe',
-                    data: [163, 203, 276, 408, 547, 729, 628]
-                }, {
-                    name: 'America',
-                    data: [18, 31, 54, 156, 339, 818, 1201]
-                }, {
-                    name: 'Oceania',
-                    data: [2, 2, 2, 6, 13, 30, 46]
-                }]
-            });
-        });
+                }
+            },
+            scale: 3,
+            fallbackToExportServer: false
+        },
+        title: {
+            text: 'Historic and Estimated Worldwide Population Distribution by Region'
+        },
+        subtitle: {
+            text: 'Source: Wikipedia.org'
+        },
+        xAxis: {
+            categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Percent'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.1f}%</b> ({point.y:,.0f} millions)<br/>',
+            shared: true
+        },
+        plotOptions: {
+            area: {
+                stacking: 'percent',
+                lineColor: '#ffffff',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#ffffff'
+                }
+            }
+        },
+        series: [{
+            name: 'Asia',
+            data: [502, 635, 809, 947, 1402, 3634, 5268]
+        }, {
+            name: 'Africa',
+            data: [106, 107, 111, 133, 221, 767, 1766]
+        }, {
+            name: 'Europe',
+            data: [163, 203, 276, 408, 547, 729, 628]
+        }, {
+            name: 'America',
+            data: [18, 31, 54, 156, 339, 818, 1201]
+        }, {
+            name: 'Oceania',
+            data: [2, 2, 2, 6, 13, 30, 46]
+        }]
+    });
+    $('#chartContainerES2').highcharts({
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Historic and Estimated Worldwide Population Growth by Region'
+        },
+        subtitle: {
+            text: 'Source: Wikipedia.org'
+        },
+        xAxis: {
+            categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Billions'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value / 1000;
+                }
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [{
+            name: 'Asia',
+            data: [502, 635, 809, 947, 1402, 3634, 5268]
+        }, {
+            name: 'Africa',
+            data: [106, 107, 111, 133, 221, 767, 1766]
+        }, {
+            name: 'Europe',
+            data: [163, 203, 276, 408, 547, 729, 628]
+        }, {
+            name: 'America',
+            data: [18, 31, 54, 156, 339, 818, 1201]
+        }, {
+            name: 'Oceania',
+            data: [2, 2, 2, 6, 13, 30, 46]
+        }]
+    });
+});
