b91727d8b575241798acc01e74ebc9ad57f4cf1b
ouzy <ousmane.mariko89@gmail.com>
2016-02-07 23:22:39 -0500

    menu sticky

--- prod/js/header.js
+++ prod/js/header.js
@@ -21,8 +21,28 @@ $(document).ready(function(){
 	max = docHeight - winHeight;
 	progressBar.attr('max', max);
 
+	
+
 	$(document).on('scroll', function(){
 	    value = $(window).scrollTop();
 	    progressBar.attr('value', value);
 	});
+});
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 50) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+    if ($(window).scrollTop() > 500) {
+        $('.main_h').addClass('stick');
+    } else {
+        $('.main_h').removeClass('stick');
+    }
+    if ($(window).scrollTop() > 600) {
+        $('.main_h').addClass('move-down');
+    } else {
+        $('.main_h').removeClass('move-down');
+    }
 });
